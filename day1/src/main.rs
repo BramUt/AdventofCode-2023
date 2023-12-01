ea04e86cd6d7501e2001d5de59be2b1542614f27
BramUt <55189626+BramUt@users.noreply.github.com>
2023-12-01 16:01:43 +0100

    Completed part 2🫠

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::env;
-use regex::Regex;
+use aho_corasick::{AhoCorasick};
 
 fn get_num(num_str: &str) -> i32 {
     match num_str {
@@ -23,16 +23,18 @@ fn main() {
     let file_name = &args[1];
     let file_content = fs::read_to_string(file_name).unwrap();
 
-    let re = Regex::new(r"([0-9]|one|two|three|four|five|six|seven|eight|nine)").unwrap();
+    // let re = Regex::new(r"([0-9]|one|two|three|four|five|six|seven|eight|nine)").unwrap();
+    let patterns = vec!["1", "2", "3", "4", "5", "6", "7", "8", "8", "9","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let ac = AhoCorasick::new(patterns.clone()).unwrap();
 
     let mut numbers: Vec<u64> = Vec::new();
     for line in file_content.lines() {
 
-        let matches: Vec<_> = re.find_iter(line).map(|m| m.as_str()).collect();
-        println!("{} {}", line, matches.join(", "));
+        let matches: Vec<usize> = ac.find_overlapping_iter(line).map(|mat| mat.pattern().as_usize()).collect();
+        println!("{} {:?}", line, matches);
 
-        let n1 = get_num(&matches[0]);
-        let n2 = get_num(&matches.last().unwrap());
+        let n1 = get_num(patterns[matches[0]]);
+        let n2 = get_num(patterns[matches.last().unwrap().to_owned()]);
         println!("{} {}", n1, n2);
         let number = format!("{}{}", n1, n2);
         println!("{}", number);
