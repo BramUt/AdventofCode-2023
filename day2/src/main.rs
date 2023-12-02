bfb622467af838be1558f48889df24b207921013
BramUt <55189626+BramUt@users.noreply.github.com>
2023-12-02 15:11:27 +0100

    Completed Day 2 😌

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,9 +4,9 @@ use std::cmp;
 
 #[derive(Debug)]
 struct Grab {
-    r: u32,
-    g: u32,
-    b: u32,
+    r: i32,
+    g: i32,
+    b: i32,
 }
 
 fn parse_grab_strs(full_grab_str: &str) -> Vec<Grab> {
@@ -34,19 +34,31 @@ fn main() {
     let file_name = &args[1];
     let file_content = fs::read_to_string(file_name).unwrap();
 
+    let mut viable_games: Vec<i32> = Vec::new();
+
     for line in file_content.lines(){
         let (prefix, suffix) = line.split_once(":").unwrap();
         let game_num: i32 = prefix.split_whitespace().last().unwrap().parse().unwrap();
         let grabs = parse_grab_strs(suffix);
 
         let (mut max_r, mut max_g, mut max_b) = (0, 0, 0);
+
         for grab in grabs {
             max_r = cmp::max(grab.r, max_r);
             max_g = cmp::max(grab.g, max_g);
-            max_b = cmp::max(grab.r, max_b);
+            max_b = cmp::max(grab.b, max_b);
         }
 
-        println!("{game_num} red:{max_r}, green:{max_g}, blue:{max_b}");
+        println!("Game {game_num}: max red:{max_r}, max green:{max_g}, max blue:{max_b}");
 
+        // Code needed for part 1:
+        // if !(max_r > 12 || max_g > 13 || max_b > 14) {
+        //     viable_games.push(game_num)
+        // }
+
+        // Code needed for part 2:
+        viable_games.push(max_r*max_g*max_b)
     }
+    println!("Viable games: {viable_games:?}");
+    println!("Sum of viable games: {}", viable_games.iter().sum::<i32>());
 }
