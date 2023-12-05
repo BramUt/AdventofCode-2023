1604245afc43c448483a69b4ff1dc7796ea63455
BramUt <55189626+BramUt@users.noreply.github.com>
2023-12-05 17:21:38 +0100

    Code works for sample but is too slow.

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,6 +4,7 @@ use std::collections::hash_map::OccupiedEntry;
 use std::env;
 use std::fs;
 use std::cmp;
+use std::str::Lines;
 
 struct ElfMap {
     source: String,
@@ -21,7 +22,10 @@ impl ElfMap {
     }
 
     fn get(&self, s: u32) -> u32 {
-        self.map.entry(s)
+        match self.map.get(&s) {
+            Some(v) => v.to_owned(),
+            None => s,
+        }
     }
 }
 
@@ -33,6 +37,11 @@ fn parse_seed_line(seed_line: &str) -> Vec<u32> {
     }
 }
 
+fn parse_map_header_line (line: &str) -> ElfMap {
+    let (source, dest) = line.strip_suffix(" map:").unwrap().split_once("-to-").unwrap();
+    ElfMap::new(source, dest)
+}
+
 fn parse_map_content_line (line: &str) -> (u32, u32, u32) {
     let numbers: Vec<u32> = match line.split_whitespace().map(|n| n.parse::<u32>()).collect() {
         Ok(nums) => nums,
@@ -42,10 +51,38 @@ fn parse_map_content_line (line: &str) -> (u32, u32, u32) {
 
 }
 
+fn parse_almanac (mut lines: Lines) -> HashMap<String, ElfMap> {
+    // Skip the first blank line.
+    let _ = lines.next().unwrap();
+    // Create the first map.
+    let mut cur_map = parse_map_header_line(lines.next().unwrap());
+
+    let mut almanac: HashMap<String, ElfMap> = HashMap::new();
+    
+    for line in lines {
+        let c: Vec<char> = line.chars().collect();
+        if line.is_empty() {
+
+        } else if c[0].is_alphabetic() {
+            almanac.insert(cur_map.source.to_owned(), cur_map);
+            cur_map = parse_map_header_line(line);
+        } else if  c[0].is_numeric() {
+            let (dest_range_start, source_range_start, range_len) = parse_map_content_line(line);
+            for i in 0..range_len {
+                cur_map.map.insert(source_range_start + i, dest_range_start + i);
+            }
+        }
+    }
+    almanac.insert(cur_map.source.to_owned(), cur_map);
+    almanac
+}
+
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_name = &args[1];
-    let file_name = "sampledata.txt";
+    // let file_name = "sampledata.txt";
 
     let file_content = match fs::read_to_string(file_name) {
         Ok(content) => content,
@@ -56,17 +93,49 @@ fn main() {
     
     let seed_line = lines.next().unwrap();
     let seed_numbers = parse_seed_line(seed_line);
-    
-    for line in lines {
-        let almanac: HashMap<String, ElfMap> = HashMap::new();
-        let mut cur_map: ElfMap;
 
-        let c: Vec<char> = line.chars().collect();
-        if c[0].is_alphabetic() {
-            let (source, dest) = line.strip_suffix(" map:").unwrap().split_once("-to-").unwrap();
-            cur_map = ElfMap::new(source, dest);
-        } else if  c[0].is_numeric() {
-            let (dest_range_start, source_range_start, range_len) = parse_map_content_line(line);
+    let almanac = parse_almanac(lines);
+
+    let mut lowest_val: Option<u32> = None;
+
+    for num in seed_numbers {
+        // let mut cur_map = match almanac.get("seed") {
+        //     Some(v) => v,
+        //     None => panic!("Unable to locate 'seed' in almanac for number {num}")
+        // };
+        // // let mut cur_val = cur_map.get(num);
+        // while true {
+        //     cur_map = match almanac.get(&cur_map.dest){
+        //         Some(v) => v,
+        //         None => panic!("Unable to locate '{}' in almanac for number {num}", cur_map.dest)
+        //     };
+        //     cur_val = cur_map.get(cur_val);
+        //     if cur_map.dest == "location" {
+        //         break;
+        //     }
+        // }
+        
+        
+        let soil = almanac.get("seed").unwrap().get(num);
+        let fert = almanac.get("soil").unwrap().get(soil);
+        let water = almanac.get("fertilizer").unwrap().get(fert);
+        let light = almanac.get("water").unwrap().get(water);
+        let temp = almanac.get("light").unwrap().get(light);
+        let humid = almanac.get("temperature").unwrap().get(temp);
+        let location = almanac.get("humidity").unwrap().get(humid);
+
+        let cur_val = location;
+        match lowest_val {
+            None => lowest_val = Some(cur_val),
+            Some(v) => {
+                if cur_val < v {
+                    lowest_val = Some(cur_val)
+                }
+            }
         }
+
     }
+
+    
+    println!("Lowest location number for initial seeds: {}", lowest_val.unwrap())
 }
